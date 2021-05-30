83da34cfab5caf8de55947e241fabc41ab5b4aa6
rachaelchermak <rchermak@css.edu>
2021-05-30 18:06:35 -0500

    is no working

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+class Pizza {
+  String _topping;
+  double _price;
+  int _size;
+  String strSize;
+  String description;
 
+  final PIZZA_PRICES = [7.99, 9.99, 12.99, 14.99];
+  final PIZZA_SIZES = ["Small", "Medium", "Large", "X-Large"];
+
+  Pizza(String this._topping, int this._size) {
+    _price = PIZZA_PRICES[_size];
+    strSize = PIZZA_SIZES[_size];
+    description = "$strSize $_topping pizza";
+  }
+}
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -28,9 +43,58 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  List<Pizza> pizzaList;
+
   void _addPizza() {
-      // TODO: display add pizza Dialog window
-    print("addPizza called");
+    final pizzaToppingTextField = TextEditingController();
+    int _sizeSelected = 1;
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+              child: SizedBox(
+                height: 200,
+                child:  Column(
+                  children: <Widget>[
+                    Text(
+                      'Toppings:',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    TextField(
+                      controller: pizzaToppingTextField,
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    DropdownButton(
+                        style: Theme.of(context).textTheme.headline4,
+                        value: _sizeSelected,
+                        items:[
+                          DropdownMenuItem(child: Text("Small"), value: 0),
+                          DropdownMenuItem(child: Text("Medium"), value: 1),
+                          DropdownMenuItem(child: Text("Large"), value: 2),
+                          DropdownMenuItem(child: Text("X-Large"), value: 3)
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            _sizeSelected = value;
+                          });
+                        }),
+                    ElevatedButton(
+                      child: Text('Add Pizza'),
+                      onPressed: () {
+                        print("Adding a pizza");
+                        setState(() {
+                          Pizza newPizza = new Pizza(pizzaToppingTextField.text, _sizeSelected);
+                          pizzaList.add(newPizza);
+                          Navigator.pop(context);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              )
+          );
+        }
+    );
   }
 
   @override
@@ -39,7 +103,14 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Text("Display List of Pizza objects here"),
+      body: ListView.builder(
+        itemCount: pizzaList.length,
+        itemBuilder: (BuildContext context, int position) {
+          return ListTile(
+            title: Text(pizzaList[position].toString()),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _addPizza,
         tooltip: 'Increment',
